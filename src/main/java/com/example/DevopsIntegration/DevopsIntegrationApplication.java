24bd08504a0f29f7113fa671dd3454622861d8ff
Devangana Choudhary <devangana.choudhary@jagrannewmedia.com>
2024-01-15 16:13:18 +0530

    test commit

--- src/main/java/com/example/DevopsIntegration/DevopsIntegrationApplication.java
+++ src/main/java/com/example/DevopsIntegration/DevopsIntegrationApplication.java
@@ -4,15 +4,19 @@ package com.example.DevopsIntegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
-	public String message(){return "Demo for Devops automation";};
+	
+	 @GetMapping("/api/demo")
+	 public String message(){return "Demo for Devops automation";};
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
